92fff947594d510e52341c6b6cc661665fd19da4
Josep Victor <thejosepvictorr@gmail.com>
2023-08-11 13:17:13 +0700

    add login admin

--- tokenmanager/src/main/java/com/lawencon/tokenmanager/controller/TokenController.java
+++ tokenmanager/src/main/java/com/lawencon/tokenmanager/controller/TokenController.java
@@ -1,28 +1,69 @@
 package com.lawencon.tokenmanager.controller;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lawencon.tokenmanager.dto.TokenReqDto;
-import com.lawencon.tokenmanager.dto.TokenResDto;
-import com.lawencon.tokenmanager.service.TokenService;
+import com.lawencon.tokenmanager.service.JwtService;
 
 @RestController
-@RequestMapping("tokens")
+@RequestMapping("token")
 public class TokenController {
-	
+
 	@Autowired
-	private TokenService tokenService;
+	private JwtService jwtService;
 	
 	@PostMapping
-	public ResponseEntity<TokenResDto> getToken(@RequestBody TokenReqDto data){
-		final TokenResDto response = tokenService.generateToken(data);
-		return new ResponseEntity<TokenResDto>(response, HttpStatus.CREATED);
+	public ResponseEntity<String> getToken(@RequestBody TokenReqDto tokenDto){
+		final Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date());
+		cal.add(Calendar.HOUR_OF_DAY, 1);
+		
+		final Map<String, Object> claims = new HashMap<>();
+		claims.put("exp", cal.getTime());
+		claims.put("id",tokenDto.getId());
+		final String response = jwtService.generateJwt(claims);
+		return new ResponseEntity<String>(response, HttpStatus.OK);
 	}
+	
+	@PostMapping("/validate")
+	public ResponseEntity<String> validateToken(){
+		final Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
+		if (auth == null || auth.getPrincipal() == null)
+			throw new RuntimeException("Invalid Login");
+		
+		
+		return new ResponseEntity<String>(auth.getPrincipal().toString(), HttpStatus.OK);
+	}
+	
 }
+//
+//@RestController
+//@RequestMapping("tokens")
+//public class TokenController {
+//	
+//	
+//	
+//	@Autowired
+//	private TokenService tokenService;
+//	
+//	@PostMapping
+//	public ResponseEntity<TokenResDto> getToken(@RequestBody TokenReqDto data){
+//		final TokenResDto response = tokenService.generateToken(data);
+//		return new ResponseEntity<TokenResDto>(response, HttpStatus.CREATED);
+//	}
+//
+//}
